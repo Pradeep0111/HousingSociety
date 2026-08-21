eeaafacf05c1530dde56a8d613cb0f6a079fbe73
Pradeep S <pradeep5049@outlook.com>
2026-08-21 12:26:07 +0530

    Initial commit

---
+++ Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class ApplicationUser : IdentityUser<Guid>
+    {
+
+    }
+}
